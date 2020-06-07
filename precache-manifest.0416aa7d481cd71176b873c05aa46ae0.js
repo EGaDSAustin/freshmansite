9e83dd4f2b621e1e50555dc7900cbec11f0e28e9
Colette Montminy <colette.montminy@gmail.com>
2020-06-07 04:19:05 -0500

    Updates

---
+++ precache-manifest.0416aa7d481cd71176b873c05aa46ae0.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae5644e9bdd1c28aa2eb7e79b49ec16a",
+    "url": "/freshmansite/index.html"
+  },
+  {
+    "revision": "1448da8c10d8b36443b0",
+    "url": "/freshmansite/static/css/main.a2a89af3.chunk.css"
+  },
+  {
+    "revision": "b1788ea8c0dc14ab6197",
+    "url": "/freshmansite/static/js/2.44aeb53d.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/freshmansite/static/js/2.44aeb53d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1448da8c10d8b36443b0",
+    "url": "/freshmansite/static/js/main.a39abc0c.chunk.js"
+  },
+  {
+    "revision": "0abcc5c3b0a0fd42b3e8",
+    "url": "/freshmansite/static/js/runtime-main.763c1931.js"
+  },
+  {
+    "revision": "93bf9b4db74c47f60e87947d92c94ace",
+    "url": "/freshmansite/static/media/ELECTRICBOOTS.93bf9b4d.ttf"
+  },
+  {
+    "revision": "e09dfb5b70508099e430a52cc93b59a2",
+    "url": "/freshmansite/static/media/KotoriRose-Regular.e09dfb5b.otf"
+  },
+  {
+    "revision": "8aea6d38539bb35593e5fd3c74e8ab4a",
+    "url": "/freshmansite/static/media/blueshape1.8aea6d38.png"
+  },
+  {
+    "revision": "ec84ed015b3d5ceb0467fc80ede3d3a4",
+    "url": "/freshmansite/static/media/blueshape2.ec84ed01.png"
+  },
+  {
+    "revision": "4f2e99caa855dce44edceca6b7ab86f0",
+    "url": "/freshmansite/static/media/blueshape3.4f2e99ca.png"
+  },
+  {
+    "revision": "dcd5628dd6ebdebd04d0cb320207bbe1",
+    "url": "/freshmansite/static/media/blueshape4.dcd5628d.png"
+  },
+  {
+    "revision": "d1f7742cd566ae27bde2e56927ab0ad9",
+    "url": "/freshmansite/static/media/blueshape5.d1f7742c.png"
+  },
+  {
+    "revision": "43db3ed8ccc143ee48777cfd889b4f6c",
+    "url": "/freshmansite/static/media/carmen.43db3ed8.jpg"
+  },
+  {
+    "revision": "2b94b9bf28c002a87494387b6fb6ceb5",
+    "url": "/freshmansite/static/media/egadscontroller2.2b94b9bf.png"
+  },
+  {
+    "revision": "149f0dd9b9e0dd6af6a47ed7f1b3e553",
+    "url": "/freshmansite/static/media/gamejam1.149f0dd9.JPG"
+  },
+  {
+    "revision": "6f674ff62fdb92b03b32b7a5ca60f583",
+    "url": "/freshmansite/static/media/gamejam2.6f674ff6.JPG"
+  },
+  {
+    "revision": "e1c182fd59dcd66b1a3fe80afd143293",
+    "url": "/freshmansite/static/media/gamejam3.e1c182fd.JPG"
+  },
+  {
+    "revision": "83003c7c3ca63409664ec35bb91b0369",
+    "url": "/freshmansite/static/media/goodboye.83003c7c.png"
+  },
+  {
+    "revision": "a21be21d28276c0afff641cee3385567",
+    "url": "/freshmansite/static/media/tetris.a21be21d.JPG"
+  },
+  {
+    "revision": "34ba2f347d2ace84543bab84c3e33b56",
+    "url": "/freshmansite/static/media/whale.34ba2f34.JPG"
+  }
+]);
