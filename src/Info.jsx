9f17c21df47905c3054cff3c92af0c626b08272b
Colette Montminy <cmontminy@utexas.edu>
2020-06-12 01:20:00 -0500

    added sidebar

---
+++ src/Info.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+
+const Info = () => {
+    return (
+        <>
+            <Typography>Hey bois :) epic gamer moment</Typography>
+        </>
+    );
+};
+
+export default Info;
